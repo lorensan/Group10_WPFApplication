7630496775f95947a44d05b0ae74db18988532a7
lorensan <lorensan90@gmail.com>
2016-06-20 13:58:18 +0000

    Add files via upload

---
+++ DatabaseOperation/ConcesionarioOperation.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PruebaGrupo10_WPF.DatabaseOperation
+{
+    class ConcesionarioOperation
+    {
+
+
+
+        public List<string> getTipos()
+        {
+            //Inicializamos la lista de string a devolver
+            List<string> tipos = new List<string>();
+
+            using (ConcesionarioDB svcContext = new ConcesionarioDB())
+            {
+                var tipQuery = (from tp in svcContext.Tipos
+                                   select tp.Descripcion).Distinct();
+
+                foreach (var a in tipQuery)
+                {
+                    tipos.Add(a.ToString());
+                }
+            }
+
+            return tipos;
+        }
+
+        public List<string> getModelosPorTipo(string tipo)
+        {
+            //Inicializamos la lista de string a devolver
+            List<string> modelos = new List<string>();
+
+            using (ConcesionarioDB concesDB = new ConcesionarioDB())
+            {
+                var tipQuery = from tp in concesDB.Tipos
+                                join md in concesDB.Modelos on tp.CodTipo equals md.CodTipo
+                                where tp.Descripcion==tipo
+                                select md.Marca;
+
+                foreach (var a in tipQuery)
+                {
+                    modelos.Add(a.ToString());
+                }
+            }
+
+            return modelos;
+        }
+
+        public void insertarRegistro(string marca, string descricpion, string ruta)
+        {
+            int cod = 0;
+            using (ConcesionarioDB concesDB = new ConcesionarioDB())
+            {
+                var tipQuery = from md in concesDB.Modelos
+                               orderby md.CodTipo ascending
+                               select md.CodTipo; 
+
+                foreach (var a in tipQuery)
+                {
+                    cod = Convert.ToInt16(a)+1;
+                }
+
+                Modelos t = new Modelos
+                {
+                    CodTipo = cod++,
+                    Marca = marca,
+                    Descripcion = descricpion,
+                    Imagen = Encoding.ASCII.GetBytes(ruta)
+                };
+
+                concesDB.Modelos.AddObject(t);
+                concesDB.SaveChanges();
+            }
+
+           
+            
+        }
+
+        /// <summary>
+        /// Modificar registro en base de datos
+        /// </summary>
+        /// <param name="cod"></param>
+        /// <param name="marca"></param>
+        /// <param name="descricpion"></param>
+        /// <param name="ruta"></param>
+        public void modificarRegistro(int cod,string marca, string descricpion, string ruta)
+        {
+
+            throw new Exception("Funcion no implementada");
+        }
+    }
+}
---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+
+namespace PruebaGrupo10_WPF
+{
+
+    
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Variables
+        DatabaseOperation.ConcesionarioOperation callQuery;
+        int cod;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            //Inicializamos las variables
+            callQuery = new DatabaseOperation.ConcesionarioOperation();
+
+            rellenarListBox();
+            CargarModelosTabla();
+        }
+        
+        
+        //***********************************************
+        //                Funciones privadas
+        //***********************************************
+        public void rellenarListBox()
+        {
+            //llamamos a la función
+            List<string> result= callQuery.getTipos();
+
+            //Rellenamos el comboBox
+            foreach (var tip in result)
+            {
+                cbTipos.Items.Add(tip.ToString());
+            }
+        }
+
+        //Accion al cambiar de comboBox
+        private void cbTipos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Limpiamos los valores de los listBox
+            mListView.Items.Clear();
+            mListbox.Items.Clear();
+
+            //llamamos a la función
+            List<string> result = callQuery.getModelosPorTipo(cbTipos.SelectedItem.ToString());
+
+            //Rellenamos el comboBox
+            foreach (var tip in result)
+            {
+                mListView.Items.Add(tip.ToString());
+                mListbox.Items.Add(tip.ToString());
+            }
+        }
+
+        //Cargamos DataGrid de Tipos
+        private void CargarModelosTabla()
+        {
+            using (ConcesionarioDB concesDB = new ConcesionarioDB())
+            {
+                var query = from md in concesDB.Modelos
+                            select new { md.CodTipo, md.Marca, md.Descripcion, md.Imagen };
+                dataGrid2.ItemsSource = query.ToList();
+            }
+           
+        }
+
+
+        //Menu para abrir ruta del fichero
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string direccion = openFileDialog.FileName;
+                string archivo = Path.GetFileName(openFileDialog.FileName);
+                txtRuta.Text = String.Concat(direccion,"\\",archivo);
+            }
+        }
+
+        //Funcion para el botón insertar
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (txtMarca.Text != "")
+            {
+                if (botonInsertar.Content.ToString() == "Insertar")
+                {
+                    //Insertamos un nuevo campo
+                    callQuery.insertarRegistro(txtMarca.Text, txtDesc.Text, txtRuta.Text);
+                    CargarModelosTabla();
+                }
+                else
+                {
+                    try
+                    {
+                        callQuery.modificarRegistro(cod, txtMarca.Text, txtDesc.Text, txtRuta.Text);
+                    }
+                    catch (Exception ex) 
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Inserte el nombre de la marca del vehiculo.");
+            }
+        }
+
+        //Cambiar el valor del boton al seleccionar el checkbox
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {                
+                botonInsertar.Content = "Editar";
+        }
+
+        private void chEdicion_Unchecked(object sender, RoutedEventArgs e)
+        {
+                botonInsertar.Content = "Insertar";
+        }
+
+
+        private void dataGrid2_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            if (chEdicion.IsChecked == true)
+            {
+                try
+                {
+                    foreach (var item in e.AddedCells)
+                    {
+                        var col = item.Column as DataGridColumn;
+
+                        //MessageBox.Show("" + col.Header);
+                        var fc = col.GetCellContent(item.Item);
+
+                        switch (Convert.ToString(col.Header))
+                        {
+                            case "codTipo":
+                                cod = Convert.ToInt16((fc as TextBlock).Text);
+                                break;
+                            case "Marca":
+                                txtMarca.Text = (fc as TextBlock).Text;
+                                break;
+                            case "Descripcion":
+                                txtDesc.Text = (fc as TextBlock).Text;
+                                break;
+                            case "Imagen":
+                                //txtRuta.Text=(fc as TextBlock).Text;
+                                break;
+                        }
+
+                    }
+                }catch(Exception){
+                }
+            }
+
+           
+        }
+    }
+}
